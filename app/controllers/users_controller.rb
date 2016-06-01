8f93f433098b510fd3b7d6cbbc174cc4a5deb374
Tyler Garcia <t-garcia15@hotmail.com>
2016-05-31 22:50:23 -0700

    paginate in alphabetical order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
-    @users = User.paginate(:page => params[:page], :per_page => 10)
+    @users = User.paginate(:page => params[:page], :per_page => 10).order(:name)
+    sort_by = params[:sort_by]
   end
 
   def about
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ActiveRecord::Base
+	
+	def self.filtered_by_age(opts = {})
+     min = opts[:min]
+     max = opts[:max]
+     user = User.arel_table
+
+     if min && max
+       self.where(:age => min..max)
+     elsif min && !max
+       self.where(user[:age].gt(min))
+     elsif max && !min
+       self.where(user[:age].lt(max))
+     else
+       self.all
+     end
+   end
+
 	def index
 	end
 end
